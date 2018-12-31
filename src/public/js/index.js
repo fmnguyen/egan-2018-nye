c098ba6916ea5f6386e5561cd864d1a7f1eddd4f
fmnguyen <fmnguyen@uw.edu>
2018-12-31 03:38:22 -0800

    confetti bruh

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,50 +1,86 @@
-/**
- * Returns a promise representing the response to a post request to a given url
- * @param {} data
- */
-const postFormData = (url, data) => {
-    return axios.post(url, data)
-        .then((response) => {
-            console.log(response);
-        }).catch((error) => {
-            console.log(error)
+(function () {
+    /**
+     * Returns a promise representing the response to a post request to a given url
+     * @param {} data
+     */
+    const postFormData = (url, data) => {
+        return axios.post(url, data)
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error)
+            });
+    };
+
+    /**
+     * Takes an identifier for a form (class or ID), and returns a JSON object representing all the elements of that form
+     * @param String formTag
+     */
+    const formToJson = (formTag) => {
+        const temp = {};
+        $.each($(formTag).serializeArray(), function (i, field) {
+            temp[field.name] = field.value;
         });
-};
+        return temp;
+    };
 
-/**
- * Takes an identifier for a form (class or ID), and returns a JSON object representing all the elements of that form
- * @param String formTag
- */
-const formToJson = (formTag) => {
-    const temp = {};
-    $.each($(formTag).serializeArray(), function (i, field) {
-        temp[field.name] = field.value;
-    });
-    return temp;
-};
+    $(document).ready(() => {
+        const socket = io.connect('http://localhost');
+        // const socket = io.connect('http://egan.house');
+        //const socket = io.connect('http://173.250.200.18:843/'); //Make sure this is changed to host IP + :843
 
-$(document).ready(() => {
-    const socket = io.connect('http://localhost');
-    // const socket = io.connect('http://egan.house');
-    //const socket = io.connect('http://173.250.200.18:843/'); //Make sure this is changed to host IP + :843
+        SetGlobals();
+        InitializeButton();
+        InitializeConfetti();
 
-    $('.form').submit((e) => {
-        e.preventDefault();
-
-        // Take the form elements and convert it into a json object
-        const data = formToJson('.form');
-        console.log(data);
-
-        // General pattern:
-        // 1. On some event, do socket.emit with a protocol (read some string) as the first parameter, data as second param
-        //     This protocol responds to a similar socket.on() event that the server is listening to
-        // 2. Have a socket listening to an event (socket.on) that the server emits and do something in response
-        socket.emit('submitPacket', {
-            nickname: data.nickname,
-            message: data.submitText
+        $(window).resize(function () {
+            W = window.innerWidth;
+            H = window.innerHeight;
+            canvas.width = W;
+            canvas.height = H;
         });
-        //const res = postFormData(data);
-        return false;
+
+        setTimeout(() => {
+            DeactivateConfetti();
+        }, 3000);
+
+
+
+        $('.form').submit((e) => {
+            e.preventDefault();
+
+            // Take the form elements and convert it into a json object
+            const data = formToJson('.form');
+            console.log(data);
+
+            if (data.nickname === '' || data.submitText === '') {
+                return false;
+            } else {
+                socket.emit('submitPacket', {
+                    nickname: data.nickname,
+                    message: data.submitText
+                });
+                console.log('socket emit event');
+
+                $('.form--textBox').each(function (el) {
+                    $(this).val('');
+                });
+
+                RestartConfetti();
+            }
+
+            // General pattern:
+            // 1. On some event, do socket.emit with a protocol (read some string) as the first parameter, data as second param
+            //     This protocol responds to a similar socket.on() event that the server is listening to
+            // 2. Have a socket listening to an event (socket.on) that the server emits and do something in response
+
+            //const res = postFormData(data);
+            RestartConfetti();
+            setTimeout(() => {
+                DeactivateConfetti();
+            }, 2000);
+            return false;
+        });
     });
 
     // Do something in response to a server emitted event
@@ -52,4 +88,210 @@ $(document).ready(() => {
     // socket.on('someoneSubmittedSomething', (msg) => {
     //     $('.container--messages').append($('<li>').text(msg.nickname + ': ' + msg.message));
     // });
-});
+
+
+    // globals
+    var canvas;
+    var ctx;
+    var W;
+    var H;
+    var mp = 300; //max particles
+    var particles = [];
+    var angle = 0;
+    var tiltAngle = 0;
+    var confettiActive = true;
+    var animationComplete = true;
+    var deactivationTimerHandler;
+    var reactivationTimerHandler;
+    var animationHandler;
+
+    // objects
+
+    var particleColors = {
+        colorOptions: ["DodgerBlue", "OliveDrab", "Gold", "pink", "SlateBlue", "lightblue", "Violet", "PaleGreen", "SteelBlue", "SandyBrown", "Chocolate", "Crimson"],
+        colorIndex: 0,
+        colorIncrementer: 0,
+        colorThreshold: 10,
+        getColor: function () {
+            if (this.colorIncrementer >= 10) {
+                this.colorIncrementer = 0;
+                this.colorIndex++;
+                if (this.colorIndex >= this.colorOptions.length) {
+                    this.colorIndex = 0;
+                }
+            }
+            this.colorIncrementer++;
+            return this.colorOptions[this.colorIndex];
+        }
+    }
+
+    function confettiParticle(color) {
+        this.x = Math.random() * W; // x-coordinate
+        this.y = (Math.random() * H) - H; //y-coordinate
+        this.r = RandomFromTo(10, 30); //radius;
+        this.d = (Math.random() * mp) + 10; //density;
+        this.color = color;
+        this.tilt = Math.floor(Math.random() * 10) - 10;
+        this.tiltAngleIncremental = (Math.random() * 0.07) + .05;
+        this.tiltAngle = 0;
+
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.lineWidth = this.r / 2;
+            ctx.strokeStyle = this.color;
+            ctx.moveTo(this.x + this.tilt + (this.r / 4), this.y);
+            ctx.lineTo(this.x + this.tilt, this.y + this.tilt + (this.r / 4));
+            return ctx.stroke();
+        }
+    }
+
+    function InitializeButton() {
+        $('#stopButton').click(DeactivateConfetti);
+        $('#startButton').click(RestartConfetti);
+    }
+
+    function SetGlobals() {
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        W = window.innerWidth;
+        H = window.innerHeight;
+        canvas.width = W;
+        canvas.height = H;
+    }
+
+    function InitializeConfetti() {
+        particles = [];
+        animationComplete = false;
+        for (var i = 0; i < mp; i++) {
+            var particleColor = particleColors.getColor();
+            particles.push(new confettiParticle(particleColor));
+        }
+        StartConfetti();
+    }
+
+    function Draw() {
+        ctx.clearRect(0, 0, W, H);
+        var results = [];
+        for (var i = 0; i < mp; i++) {
+            (function (j) {
+                results.push(particles[j].draw());
+            })(i);
+        }
+        Update();
+
+        return results;
+    }
+
+    function RandomFromTo(from, to) {
+        return Math.floor(Math.random() * (to - from + 1) + from);
+    }
+
+
+    function Update() {
+        var remainingFlakes = 0;
+        var particle;
+        angle += 0.1;
+        tiltAngle += 0.1;
+
+        for (var i = 0; i < mp; i++) {
+            particle = particles[i];
+            if (animationComplete) return;
+
+            if (!confettiActive && particle.y < -15) {
+                particle.y = H + 100;
+                continue;
+            }
+
+            stepParticle(particle, i);
+
+            if (particle.y <= H) {
+                remainingFlakes++;
+            }
+            CheckForReposition(particle, i);
+        }
+
+        if (remainingFlakes === 0) {
+            StopConfetti();
+        }
+    }
+
+    function CheckForReposition(particle, index) {
+        if ((particle.x > W + 20 || particle.x < -20 || particle.y > H) && confettiActive) {
+            if (index % 5 > 0 || index % 2 == 0) //66.67% of the flakes
+            {
+                repositionParticle(particle, Math.random() * W, -10, Math.floor(Math.random() * 10) - 20);
+            } else {
+                if (Math.sin(angle) > 0) {
+                    //Enter from the left
+                    repositionParticle(particle, -20, Math.random() * H, Math.floor(Math.random() * 10) - 20);
+                } else {
+                    //Enter from the right
+                    repositionParticle(particle, W + 20, Math.random() * H, Math.floor(Math.random() * 10) - 20);
+                }
+            }
+        }
+    }
+
+    function stepParticle(particle, particleIndex) {
+        particle.tiltAngle += particle.tiltAngleIncremental;
+        particle.y += (Math.cos(angle + particle.d) + 3 + particle.r / 2) / 2;
+        particle.x += Math.sin(angle);
+        particle.tilt = (Math.sin(particle.tiltAngle - (particleIndex / 3))) * 15;
+    }
+
+    function repositionParticle(particle, xCoordinate, yCoordinate, tilt) {
+        particle.x = xCoordinate;
+        particle.y = yCoordinate;
+        particle.tilt = tilt;
+    }
+
+    function StartConfetti() {
+        W = window.innerWidth;
+        H = window.innerHeight;
+        canvas.width = W;
+        canvas.height = H;
+        (function animloop() {
+            if (animationComplete) return null;
+            animationHandler = requestAnimFrame(animloop);
+            return Draw();
+        })();
+    }
+
+    function ClearTimers() {
+        clearTimeout(reactivationTimerHandler);
+        clearTimeout(animationHandler);
+    }
+
+    function DeactivateConfetti() {
+        confettiActive = false;
+        ClearTimers();
+    }
+
+    function StopConfetti() {
+        animationComplete = true;
+        if (ctx == undefined) return;
+        ctx.clearRect(0, 0, W, H);
+    }
+
+    function RestartConfetti() {
+        ClearTimers();
+        StopConfetti();
+        reactivationTimerHandler = setTimeout(function () {
+            confettiActive = true;
+            animationComplete = false;
+            InitializeConfetti();
+        }, 100);
+
+    }
+
+    window.requestAnimFrame = (function () {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback) {
+                return window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+})();
